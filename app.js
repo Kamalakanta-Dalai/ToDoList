7468648789f657bd9192a8c7740291767acc269e
Kamalakanta Dalai <dalaikamalakanta52@gmail.com>
2024-01-25 01:13:42 +0530

    slight change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.set("view engine", "ejs");
